efd3e08f746c71db9d2ae71c6f26592224705157
TravyTheDev <traviskleckley1@gmail.com>
2024-10-03 19:21:22 +0900

    notifications-dont-need-to-be-a-slice

--- server/service/ws/invite.go
+++ server/service/ws/invite.go
@@ -11,8 +11,7 @@ import (
 
 type Broker struct {
 	// users is a map where the key is the user ID and the value
-	// is a slice of channels to connections for that user ID
-	users map[string][]chan []byte
+	users map[string]chan []byte
 	// actions is a channel of functions to cal in the brokers
 	// go routine. The broker executes everything in that single
 	// go routine to avoid data races.
@@ -29,7 +28,7 @@ type NotifMessage struct {
 
 func NewBroker(secretKey string) *Broker {
 	return &Broker{
-		users:    make(map[string][]chan []byte),
+		users:    make(map[string]chan []byte),
 		actions:  make(chan func()),
 		jwtMaker: jwt.NewJWTMaker(secretKey),
 	}
@@ -49,7 +48,7 @@ func (b *Broker) listen() {
 
 func (b *Broker) addUserChan(id string, ch chan []byte) {
 	b.actions <- func() {
-		b.users[id] = append(b.users[id], ch)
+		b.users[id] = ch
 	}
 }
 
@@ -60,23 +59,8 @@ func (b Broker) removeUserChan(id string, ch chan []byte) {
 		}
 	}()
 	b.actions <- func() {
-		channels := b.users[id]
-		i := 0
-		for _, channel := range channels {
-			if channel != ch {
-				channels[i] = channel
-				i = i + 1
-			}
-			if i == 0 {
-				delete(b.users, id)
-			} else {
-				b.users[id] = channels[:1]
-			}
-			// Close channel to break loop at beginning
-			// this must be done to ensure broker does not send to
-			// closed goroutine
-			close(ch)
-		}
+		delete(b.users, id)
+		close(ch)
 	}
 }
 
@@ -123,8 +107,6 @@ func (b *Broker) broadcastMessage(w http.ResponseWriter, r *http.Request) {
 
 func (b *Broker) sendToUser(id string, data []byte) {
 	b.actions <- func() {
-		for _, ch := range b.users[id] {
-			ch <- data
-		}
+		b.users[id] <- data
 	}
 }
